84d7a4fd688f65a97498140873b05cfed6eeebde
Rufat Hajizada <hajizada.rufat@gmail.com>
2024-08-04 19:20:12 -0400

    Fixing install-fonts.sh script

--- scripts/install-fonts.sh
+++ scripts/install-fonts.sh
@@ -1,19 +1,19 @@
 # Install Nerd fonts
 cd /tmp
 echo "Installing fonts"
-mkdir -p ~/.local/share /dev/null 2>/dev/null
-mkdir -p ~/.local/share/fonts /dev/null 2>/dev/null
+mkdir -p $HOME/.local/share /dev/null 2>/dev/null
+mkdir -p $HOME/.local/share/fonts /dev/null 2>/dev/null
 wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip
 unzip CascadiaMono.zip -d CascadiaFont
-cp CascadiaFont/*.ttf $(HOME)/.local/share/fonts
+cp CascadiaFont/*.ttf $HOME/.local/share/fonts
 rm -rf CascadiaMono.zip CascadiaFont
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip
 unzip JetBrainsMono.zip -d JetBrainsMono
-cp JetBrainsMono/*.ttf $(HOME)/.local/share/fonts
+cp JetBrainsMono/*.ttf $HOME/.local/share/fonts
 rm -rf JetBrainsMono.zip JetBrainsMono
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Meslo.zip
 unzip Meslo.zip -d Meslo
-cp Meslo/*ttf $(HOME)/.local/share/fonts
+cp Meslo/*ttf $HOME/.local/share/fonts
 rm -rf Meslo.zip Meslo
 cd --
 fc-cache
