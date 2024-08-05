c503d935ff946c0d38e38ba752ffacb15016b29c
Rufat Hajizada <hajizada.rufat@gmail.com>
2024-08-05 00:29:19 -0400

    Hopefully finished the gnome extension configuration, added script to enable services

---
+++ scripts/enable-services.sh
@@ -0,0 +1,6 @@
+sudo systemctl enable gdm
+sudo systemctl start gdm
+sudo systemctl enable NetworkManager
+sudo systemctl enable NetworkManager
+sudo systemctl enable bluetooth.service
+sudo systemctl start bluetooth.service
--- scripts/install-gnome-extensions.sh
+++ scripts/install-gnome-extensions.sh
@@ -1,4 +1,109 @@
 HOME=$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")
 export PATH="$HOME/.local/bin:$PATH"
+echo "Installing 'gnome-extensions-cli'"
 pipx install gnome-extensions-cli --system-site-packages
-cat $HOME/requirements/pacman/packages.txt | xargs gext install
+echo "Installing gnome extensions"
+gext install clipboard-indicator@tudmotu.com
+gext install undecorate@sun.wxg@gmail.com
+gext install space-bar@luchrioh
+gext install blur-my-shell@aunetx
+gext install just-perfection-desktop@just-perfection
+gext install tactile@lundal.io
+gext install no-titlebar-when-maximized@alec.ninja
+gext install apps-menu@gnome-shell-extensions.gcampax.github.com
+gext install auto-move-windows@gnome-shell-extensions.gcampax.github.com
+gext install drive-menu@gnome-shell-extensions.gcampax.github.com
+gext install launch-new-instance@gnome-shell-extensions.gcampax.github.com
+gext install light-style@gnome-shell-extensions.gcampax.github.com
+gext install native-window-placement@gnome-shell-extensions.gcampax.github.com
+gext install places-menu@gnome-shell-extensions.gcampax.github.com
+gext install screenshot-window-sizer@gnome-shell-extensions.gcampax.github.com
+gext install system-monitor@gnome-shell-extensions.gcampax.github.com
+gext install user-theme@gnome-shell-extensions.gcampax.github.com
+gext install window-list@gnome-shell-extensions.gcampax.github.com
+gext install windowsNavigator@gnome-shell-extensions.gcampax.github.com
+gext install workspace-indicator@gnome-shell-extensions.gcampax.github.com
+echo "Compiling extension setting schemas"
+sudo cp ~/.local/share/gnome-shell/extensions/clipboard-indicator\@tudmotu.com/schemas/org.gnome.shell.extensions.clipboard-indicator.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/space-bar\@luchrioh/schemas/org.gnome.shell.extensions.space-bar.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/blur-my-shell\@aunetx/schemas/org.gnome.shell.extensions.blur-my-shell.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/just-perfection-desktop\@just-perfection/schemas/org.gnome.shell.extensions.just-perfection.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/tactile\@lundal.io/schemas/org.gnome.shell.extensions.tactile.gschema.xml /usr/share/glib-2.0/schemas/
+sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
+echo "Configuring extensions"
+gsettings set org.gnome.shell.extensions.tactile col-0 1
+gsettings set org.gnome.shell.extensions.tactile col-1 2
+gsettings set org.gnome.shell.extensions.tactile col-2 1
+gsettings set org.gnome.shell.extensions.tactile col-3 0
+gsettings set org.gnome.shell.extensions.tactile row-0 1
+gsettings set org.gnome.shell.extensions.tactile row-1 1
+gsettings set org.gnome.shell.extensions.tactile gap-size 32
+gsettings set org.gnome.shell.extensions.just-perfection animation 2
+gsettings set org.gnome.shell.extensions.just-perfection dash-app-running true
+gsettings set org.gnome.shell.extensions.just-perfection workspace true
+gsettings set org.gnome.shell.extensions.just-perfection workspace-popup false
+gsettings set org.gnome.shell.extensions.blur-my-shell.appfolder blur false
+gsettings set org.gnome.shell.extensions.blur-my-shell.lockscreen blur false
+gsettings set org.gnome.shell.extensions.blur-my-shell.screenshot blur false
+gsettings set org.gnome.shell.extensions.blur-my-shell.window-list blur false
+gsettings set org.gnome.shell.extensions.blur-my-shell.panel brightness 0.6
+gsettings set org.gnome.shell.extensions.blur-my-shell.panel sigma 30
+gsettings set org.gnome.shell.extensions.blur-my-shell.panel pipeline 'pipeline_default'
+gsettings set org.gnome.shell.extensions.blur-my-shell.overview blur true
+gsettings set org.gnome.shell.extensions.blur-my-shell.overview pipeline 'pipeline_default'
+gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock blur true
+gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock brightness 0.6
+gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock sigma 30
+gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock static-blur true
+gsettings set org.gnome.shell.extensions.blur-my-shell.dash-to-dock style-dash-to-dock 0
+gsettings set org.gnome.shell.extensions.space-bar.behavior smart-workspace-names false
+gsettings set org.gnome.shell.extensions.space-bar.shortcuts enable-activate-workspace-shortcuts false
+gsettings set org.gnome.shell.extensions.space-bar.shortcuts enable-move-to-workspace-shortcuts true
+gsettings set org.gnome.shell.extensions.space-bar.shortcuts open-menu "@as []"
+gsettings set org.gnome.mutter center-new-windows true
+gsettings set org.gnome.desktop.interface monospace-font-name 'CaskaydiaMono Nerd Font 10'
+gsettings set org.gnome.desktop.wm.preferences button-layout "close,minimize,maximize:"
+gsettings set org.gnome.desktop.wm.keybindings close "['<Super>w']"
+gsettings set org.gnome.desktop.wm.keybindings maximize "['<Super>Up']"
+gsettings set org.gnome.desktop.wm.keybindings begin-resize "['<Super>BackSpace']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys next "['<Shift>AudioPlay']"
+gsettings set org.gnome.desktop.wm.keybindings toggle-fullscreen "['<Shift>F11']"
+gsettings set org.gnome.mutter dynamic-workspaces false
+gsettings set org.gnome.desktop.wm.preferences num-workspaces 6
+gsettings set org.gnome.shell.keybindings switch-to-application-1 "['<Alt>1']"
+gsettings set org.gnome.shell.keybindings switch-to-application-2 "['<Alt>2']"
+gsettings set org.gnome.shell.keybindings switch-to-application-3 "['<Alt>3']"
+gsettings set org.gnome.shell.keybindings switch-to-application-4 "['<Alt>4']"
+gsettings set org.gnome.shell.keybindings switch-to-application-5 "['<Alt>5']"
+gsettings set org.gnome.shell.keybindings switch-to-application-6 "['<Alt>6']"
+gsettings set org.gnome.shell.keybindings switch-to-application-7 "['<Alt>7']"
+gsettings set org.gnome.shell.keybindings switch-to-application-8 "['<Alt>8']"
+gsettings set org.gnome.shell.keybindings switch-to-application-9 "['<Alt>9']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/']"
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source "@as []"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'ulauncher-toggle'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'ulauncher-toggle'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>space'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'Flameshot'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'sh -c -- "flameshot gui"'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding '<Control>Print'
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['<Super>7']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "['<Super>8']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 "['<Super>9']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>0']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1 "['<Super><Shift>1']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2 "['<Super><Shift>2']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 "['<Super><Shift>3']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-4 "['<Super><Shift>4']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-5 "['<Super><Shift>5']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-6 "['<Super><Shift>6']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-7 "['<Super><Shift>7']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-8 "['<Super><Shift>8']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-9 "['<Super><Shift>9']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 "['<Super><Shift>0']"
