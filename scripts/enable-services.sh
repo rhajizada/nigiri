80a9f0c9cc19f83dfecbfc52db2095b109129ae5
Rufat Hajizada <hajizada.rufat@gmail.com>
2024-08-06 01:04:07 -0400

    Remvoed unused services, added noconfirm to initial pacman update

--- scripts/enable-services.sh
+++ scripts/enable-services.sh
@@ -4,5 +4,3 @@ sudo systemctl enable NetworkManager
 sudo systemctl enable NetworkManager
 sudo systemctl enable bluetooth.service
 sudo systemctl start bluetooth.service
-sudo systemctl enable appimagelauncherd
-sudo systemctl start appimagelauncherd
--- scripts/install-pacman-packages.sh
+++ scripts/install-pacman-packages.sh
@@ -1,4 +1,4 @@
 NIGIRI=$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")
 echo "Installing pacman packages"
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 cat $NIGIRI/requirements/pacman/packages.txt | xargs sudo pacman -S --needed --noconfirm
