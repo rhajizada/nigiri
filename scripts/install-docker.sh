daa9e2ea1a5462774c675f96dc4ec6cffcd03ee2
Rufat Hajizada <hajizada.rufat@gmail.com>
2024-08-06 00:41:43 -0400

    Fixing install docker script

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -3,6 +3,14 @@ sudo pacman -S --noconfirm docker docker-compose
 sudo usermod -aG docker ${USER}
 sudo systemctl enable docker.service
 # Use local logging driver - it's more efficient and uses compression by default.
-echo '{"log-driver":"local","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json >/dev/null
+cat <<EOL >/etc/docker/daemon.json
+{
+  "log-driver": "local",
+  "log-opts": {
+    "max-size": "10m",
+    "max-file": "5"
+  }
+}
+EOL
 echo "Installing 'lazydocker'"
 yay -S lazydocker
