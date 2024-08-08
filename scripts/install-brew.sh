1dbabce4ca7166d1baa8d44b9c79b8656ccc06c3
Rufat Hajizada <hajizada.rufat@gmail.com>
2024-08-08 01:32:32 -0400

    Fixing install brew script

--- scripts/install-brew.sh
+++ scripts/install-brew.sh
@@ -1,6 +1,7 @@
 mkdir -p /tmp/brew
 cd /tmp/brew
 wget https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
-source install.sh
+chmod +x install.sh
+./install.sh
 cd --
 rm -rf /tmp/brew
