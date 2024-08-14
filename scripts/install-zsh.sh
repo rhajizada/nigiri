b50f21f685e463536ac467f25493ad301223aaf8
Rufat Hajizada <hajizada.rufat@gmail.com>
2024-08-14 01:20:05 -0400

    Removing comment from zsh installer script

--- scripts/install-zsh.sh
+++ scripts/install-zsh.sh
@@ -5,7 +5,6 @@ else
   echo "Zsh is already installed"
 fi
 
-# Check if Zsh is already the default shell
 if [ "$SHELL" != "/bin/zsh" ]; then
   echo "Setting Zsh as the default shell"
   chsh -s /bin/zsh
