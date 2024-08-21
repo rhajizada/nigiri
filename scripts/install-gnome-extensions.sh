5764d57665b45ad8d3a4f231f749750bb42530bc
Rufat Hajizada <hajizada.rufat@gmail.com>
2024-08-21 02:03:34 -0400

    Setting dark mode as default in gnome

--- scripts/install-gnome-extensions.sh
+++ scripts/install-gnome-extensions.sh
@@ -106,6 +106,7 @@ gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-7 "['<Super><Sh
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-8 "['<Super><Shift>8']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-9 "['<Super><Shift>9']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 "['<Super><Shift>0']"
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 if [ ! -f $HOME/.config/autostart/ulauncher.desktop ]; then
   echo "Adding ulauncher desktop entry"
   mkdir -p $HOME/.config/autostart
