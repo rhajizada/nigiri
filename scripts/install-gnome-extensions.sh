a006abb45b174100573216981563d6590cbe7121
Rufat Hajizada <hajizada.rufat@gmail.com>
2025-02-19 23:01:08 -0500

    installing aws command line, enabling fractal scaling in gnome

--- scripts/install-gnome-extensions.sh
+++ scripts/install-gnome-extensions.sh
@@ -30,6 +30,7 @@ sudo cp $HOME/.local/share/gnome-shell/extensions/just-perfection-desktop\@just-
 sudo cp $HOME/.local/share/gnome-shell/extensions/tactile\@lundal.io/schemas/org.gnome.shell.extensions.tactile.gschema.xml /usr/share/glib-2.0/schemas/
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
 echo "Configuring extensions"
+gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
 gsettings set org.gnome.shell.extensions.tactile col-0 1
 gsettings set org.gnome.shell.extensions.tactile col-1 2
 gsettings set org.gnome.shell.extensions.tactile col-2 1
